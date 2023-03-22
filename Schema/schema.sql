69769ae52ba9af06d718b8386696c1322b9690bd
Donald Akite <donaldakite27@gmail.com>
2023-03-22 22:54:40 +0000

    Add schema for Books

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -4,11 +4,22 @@ CREATE TABLE Item (
     author VARCHAR(255),
     label VARCHAR(255),
     publish_date VARCHAR(255),
-    archived BOOLEAN,
+    archived BOOLEAN
 )
 
 CREATE TABLE Genre (
    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
    name VARCHAR(255),
-   items INT FOREIGN KEY REFERENCES item(id),
+   items INT FOREIGN KEY REFERENCES item(id)
+)
+
+CREATE TABLE Books (
+   id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+   author_id INT NULL REFERENCES authors(id),
+   genre_id INT NULL REFERENCES genres(id),
+   label_id INT NULL REFERENCES labels(id)
+   publisher VARCHAR(255),
+   publish_date DATE NOT NULL,
+   cover_state VARCHAR(255),
+   archived BOOLEAN NOT NULL
 )
