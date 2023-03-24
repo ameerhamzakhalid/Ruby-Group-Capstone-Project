843929a421c386c9b08d060dced68c5f967d0e96
Donald Akite <donaldakite27@gmail.com>
2023-03-24 20:01:02 +0000

    if typo

--- spec/book_speck.rb
+++ spec/book_speck.rb
@@ -7,7 +7,7 @@ describe Book do
       @book = Book.new('Crown Business', 'good', Date.parse('2022/12/12'))
     end
 
-    it 'should create an instance fo Book class' do
+    it 'should create an instance of Book class' do
       expect(@book).to be_an_instance_of(Book)
     end
   end
