ebc3f3c7178d96837181c67820316910faef4989
ameerhamzakhalid <ameerhamza.khalid1997@gmail.com>
2023-03-25 01:48:02 +0500

    add method ,savedata ,unit test and refactor code
        data preserved by saving collections in .json files.
        music_albums table (add all properties and associations from the parent Item class as table columns)
        genres table
    
        Refactor code
        Added unit tests for methods
    Added unit-tests for methods of author and game classes
    Removed linter errors

--- main.rb
+++ main.rb
@@ -1,52 +1,85 @@
-def run_first(option)
-  case option
-  when 1 # list
-  when 2 # list
-  when 3 # list
-  when 4 # list
-  when 5 # list
-  when 6 # list
-  else
-    run_second(option)
+require_relative 'game/game'
+require_relative 'game/game_controller'
+require_relative 'book/book'
+require_relative 'book/book_controller'
+require_relative 'music_album/music_album'
+require_relative 'music_album/music_album_controller'
+require_relative 'author/author'
+require_relative 'author/author_controller'
+require_relative 'label/label'
+require_relative 'label/label_controller'
+require_relative 'genre/genre'
+require_relative 'genre/genre_controller'
+
+class Main
+  include BooksController
+  include GamesController
+  include MusicAlbumsController
+  include LabelsController
+  include GenresController
+  include AuthorsController
+
+  def initialize
+    @books = load_books
+    @labels = load_labels
+    @genres = load_genres
+  end
+
+  def user_input(message)
+    print message
+    gets.chomp
+  end
+
+  def start
+    puts '-' * 50
+    puts '⭐  Welcome to your catalog of things! ⭐'
+    loop do
+      puts '-' * 50
+      puts '
+          1- List all books
+          2- List all music albums
+          3- List of games
+          4- List all genres
+          5- List all labels
+          6- List all authors
+          7- Add a book
+          8- Add a music album
+          9- Add a game
+          10- Quit'
+
+      input = user_input('Choose an option: ').to_i
+
+      break if input == 10
+
+      options(input)
+    end
+    store_books(@books)
+    store_labels(@labels)
+    store_genres(@genres)
+  end
+
+  def options(input)
+    case input
+    when 1
+      list_books
+    when 2
+      list_music_albums
+    when 3
+      list_games
+    when 4
+      list_genres
+    when 5
+      list_labels
+    when 6
+      list_authors
+    when 7
+      add_book
+    when 8
+      add_music_album
+    when 9
+      add_game
+    else
+      puts 'Please choose a valid number!'
+    end
   end
 end
-
-def run_second(option)
-  case option
-  when 7 # list
-  when 8 # Add
-  when 9 # Add
-  else
-    puts 'Command Not Found'
-  end
-end
-
-def ask_option
-  options = [
-    'Please select an option by entering the following number:',
-    '1. List all books',
-    '2. List all music albums',
-    '3. List of games',
-    '4. List all genres',
-    '5. List all labels',
-    '6. List all authors',
-    '7. List all sources',
-    '8. Add a book',
-    '9. Add a music album',
-    '10. Add a game',
-    '11. Exit'
-  ]
-  puts options
-  choice = gets.chomp.to_i
-  return puts 'Thank you for using the app' unless choice != 11
-
-  run_first(choice)
-  ask_option
-end
-
-def main
-  puts 'Welcome to the Catalog of my things app'
-  ask_option
-end
-
-main
