84e3388b31abcf3ddbd276c357c9b2baf53ce2de
Donald Akite <donaldakite27@gmail.com>
2023-03-24 20:06:16 +0000

    test initial object

---
+++ spec/author_spec.rb
@@ -0,0 +1,21 @@
+require_relative './classes/author'
+
+describe 'Unit testing for Author Class' do
+  before :each do
+    @author = Author.new('Oluwatoyin', 'Olaoye')
+  end
+
+  context 'initialize object' do
+    it 'should be an instance of Author' do
+      expect(@author).to be_instance_of Author
+    end
+
+    it 'should not be an instance of Item' do
+      expect(@author).not_to be_instance_of Item
+    end
+
+    it 'should be a kind of Item' do
+      expect(@author).to be_kind_of Item
+    end
+  end
+end
